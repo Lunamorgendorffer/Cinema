d33bf38abc28e41f4c665cedc0773730ed547200
Lunamorgendorffer <nabdou68@gmail.com>
2023-04-04 15:54:02 +0200

    ajout fonction findOneById

--- controller/RealisateurController.php
+++ controller/RealisateurController.php
@@ -7,11 +7,22 @@ class RealisateurController{
 
         $dao = new DAO ();
        
-        $sql= "SELECT r.prenom, r.nom, r.age From realisateur r "; 
+        $sql= "SELECT r.id_realisateur, r.prenom, r.nom From realisateur r "; 
 
         $realisateurs= $dao->executerRequete($sql);
 
         require "view/realisateur/listRealisateurs.php";
+
+    }
+    public function findOneById($id){
+        $dao = new DAO ();
+       
+        $sql= "SELECT r.id_realisateur, r.prenom, r.nom, r.age From realisateur r "; 
+
+        $realisateurs= $dao->executerRequete($sql);
+
+        require "view/realisateur/detailRealisateur.php";
+
 
     }
 
