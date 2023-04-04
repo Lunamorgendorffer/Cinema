d56e8ab93e4ce9a353261a1ca7f52875f01ba30e
Lunamorgendorffer <nabdou68@gmail.com>
2023-04-04 12:21:14 +0200

    fichier detail film

---
+++ view/film/detailFilm.php
@@ -0,0 +1,19 @@
+<?php
+
+ob_start();
+
+?>
+<h1>Detail film</h1>
+<?php
+while ($film= $films->fetch()){
+    echo $film['id_film']."  ";
+    echo $film['titre']."<br>";
+}
+?>
+
+<?php
+
+$title="liste des films"; 
+$contenu = ob_get_clean(); // temporisation de sortie
+
+require "view/layout.php";
