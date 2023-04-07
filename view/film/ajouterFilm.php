41e0345bcd54cd9a0802fb8edd81dfd090861162
Lunamorgendorffer <nabdou68@gmail.com>
2023-04-07 16:57:39 +0200

    ajout style bootstrap

--- view/film/ajouterFilm.php
+++ view/film/ajouterFilm.php
@@ -4,57 +4,59 @@ ob_start();
 
 ?>
 
-<h3>Ajouter film </h3>
-<form action="index.php?action=ajouterFilm" method="post">
-
-    <p>
-        <label>titre</label>
-        <input type="text" name="titre" id="titre">
-    </p>
-    </tr>
-    
-    <p>
-        <label for="duree">duree:</label>
-       <td> <input type="int" step="any" name="duree" >
-    </p>
-    
-    <p>
-        <label>date de sortie:</label>
-        <input type="date" name="dateDeSortie" id="dateDeSortie">
-    </p>
-   
-    <p>
-        <label>note:</label>
-        <td><input type="number" name="note" id="note" >
-    </p>
-    
-    <p>
-        <label>synopsis</label>
-        <textarea name="synopsis" id="synopsis" cols="30" rows="3"></textarea>
-    </p>
-    <p>
-        <label >realisateur</label>
-        <select name="id_realisateur" id="id_realisateur">
-            <?php foreach($realisateurs->fetchAll() as $realisateur){ 
-                echo "<option value='" . $realisateur['id_realisateur'] . "'>" . $realisateur['prenom'] . " " . $realisateur['nom'] . "</option>";
-                    } ?>
-        </select>
-    </p>
-    <p>
-        <label >GENRE</label>
-        <select name="id_genre" id="id_genre">
-            <?php foreach($genres->fetchAll() as $genre){ 
-                echo "<option value='" . $genre['id_genre'] . "'>" . $genre['nom_genre'] ."</option>";
-                    } ?>
-        </select>
-    </p>
-
-    <p>
-        <input type="submit" name="submit" value="Ajouter">
-    </p>
-    
-</form>
-
+<h3 class="mb-5 mt-5" >Ajouter film </h3>
+<div class="container mb-5">
+    <div class="row g-0">
+        <form action="index.php?action=ajouterFilm" method="post">
+            <div class="row">
+                <div class="mb-5 col-4">
+                    <label  class="form-label" >Titre</label>
+                    <input class="form-control" type="text" name="titre" id="titre">
+                </div>
+                <div class="mb-5 col-4">
+                    <label class="form-label" >Durée en minutes</label>
+                    <input class="form-control" type="int" name="duree" >
+                </div>
+                <div class="mb-5 col-4">
+                    <label class="form-label">Note sur 5:</label>
+                    <input class="form-control" type="float" name="note" id="note" >
+                </div>
+            </div>
+            <div class="row d-flex justify-content-evenly">
+                <div class="mb-5 col-12">
+                        <label class="form-label" >Date de sortie:</label>
+                        <input class="form-control" type="date" name="dateDeSortie" >
+                </div>
+                <div class="mb-5 col-12">
+                    <label class="form-label">Synopsis</label>
+                    <textarea class="form-control" name="synopsis" id="synopsis" cols="30" rows="3"></textarea>
+                </div>
+            </div>
+            
+            <div class="row d-flex justify-content-evenly">
+                <div>
+                    <label class="form-label">Realisateur</label>
+                    <select class="form-select form-select-lg mb-3 col-4" name="id_realisateur" id="id_realisateur">
+                        <?php foreach($realisateurs->fetchAll() as $realisateur){ 
+                            echo "<option value='" . $realisateur['id_realisateur'] . "'>" . $realisateur['prenom'] . " " . $realisateur['nom'] . "</option>";
+                        } ?>
+                    </select>
+                </div>
+                <div>
+                    <label class="form-label" >Genre</label>
+                    <select class="form-select form-select-lg mb-3 col-4" name="id_genre" id="id_genre">
+                        <?php foreach($genres->fetchAll() as $genre){ 
+                            echo "<option value='" . $genre['id_genre'] . "'>" . $genre['nom_genre'] ."</option>";
+                        } ?>
+                    </select>
+                </div>
+            </div>
+            <div class="d-flex justify-content-evenly">
+                <button class="btn btn-primary mb-5 " name="submit" type="submit">Ajouter</button>
+            </div> 
+       </form>
+    </div>
+</div>
 
 
 <?php
