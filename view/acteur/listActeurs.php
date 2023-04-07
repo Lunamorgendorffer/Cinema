a7a10873fbbea0ae77dcf10cac5331d2b7a5b4b4
Lunamorgendorffer <nabdou68@gmail.com>
2023-04-07 17:31:39 +0200

    ajout style bootstrap

--- view/acteur/listActeurs.php
+++ view/acteur/listActeurs.php
@@ -3,21 +3,35 @@
 ob_start();
 
 ?>
-<h1>Bienvenue sur ma page acteur</h1>
-<?php
-while ($acteur = $acteurs->fetch()){?>
-    <a href="index.php?action=detailActeur&id=<?=$acteur['id_acteur']?>"><?=$acteur['prenom']." ".$acteur['nom']?></a><br>
-<?php
-    // echo $film['titre']."<br>";
-}
-?>
-<?php
-// while ($acteur= $acteurs->fetch()){
-//     echo $acteur['prenom']."    ";
-//     echo $acteur['nom']." ";
-//     echo $acteur['age']." ans <br>";
-// }
-?>
+<div class="container-fluid">
+    <h1>Bienvenue sur ma page acteur</h1>
+
+    <div class="row row-cols-1 row-cols-md-6 g-4 mt-5 d-flex justify-content-evenly">
+        <?php foreach ($acteurs->fetchAll() as $acteur){ ?>
+            <div class="card m-2 mt-4 mb-4 bg-dark">
+                <div class="card-text">
+                    <h5 class="card-title text-center"><a href="index.php?action=detailActeur&id=<?=$acteur['id_acteur']?>"><?=$acteur['prenom']." ".$acteur['nom']?></a></h5><br>
+                </div>
+                <hr class="bg-light">
+                <div class="dropdown d-flex justify-content-evenly">
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <button type="button" class="btn btn-lg"><a href="index.php?action=deletActeur&id=<?=$acteur['id_acteur']?>">Supprimer</a></button>
+                    </ul>
+                </div>
+            </div>
+        <?php }?>
+    </div>
+    <div class="row">
+        <div class="d-flex justify-content-evenly">
+            <a href="index.php?action=ajouterActeur" class="btn btn-primary">Ajouter un acteur</a>
+        </div>
+    </div>
+</div>
+
+
+    
+
+
 
 <?php
 
