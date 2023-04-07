bf7db5b753321a9cf4c404b4a5280f8412ccfcec
Lunamorgendorffer <nabdou68@gmail.com>
2023-04-07 14:26:47 +0200

    style bootstrap

--- view/film/listFilms.php
+++ view/film/listFilms.php
@@ -5,15 +5,23 @@ ob_start();
 ?>
 <h1>Bienvenue sur ma page film</h1>
 
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title"><?php while ($film= $films->fetch()){?>
-        <a href="index.php?action=detailFilm&id=<?=$film['id_film']?>"><?=$film['titre']?></a><br>
-        <?php } ?>
-    </h5>
-    <a href="index.php?action=pageAjouter" class="btn btn-primary">Ajouter film</a>
-  </div>
+<div class="row row-cols-md-5 g-4 mt-5 d-flex justify-content-evenly">
+  <?php foreach ($films->fetchAll() as $film){?>
+    <div class="card m-2 mt-4 mb-4 bg-dark">
+      <div class="card-text">
+        <h5 class="card-title text-center">
+          <a href="index.php?action=detailFilm&id=<?= $film['id_film'] ?>"><?= $film['titre'] ?></a>
+        </h5>
+      </div>
+      <hr class="bg-light">
+      <div class="dropdown d-flex justify-content-evenly">
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+          <button type="button" class="btn btn-lg "><a href="">Supprimer </a></button>
+        </ul>
+      </div>
+    </div>
+  <?php } ?>
+  <a href="index.php?action=pageAjouter" class="btn btn-primary">Ajouter film</a>
 </div>
 
 <?php
