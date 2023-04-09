f9c75e0b03de8c077b11675f10b1374bdc778160
Lunamorgendorffer <nabdou68@gmail.com>
2023-04-09 17:53:21 +0200

    creation form

--- view/genre/ajouterGenre.php
+++ view/genre/ajouterGenre.php
@@ -5,7 +5,25 @@ ob_start();
 
 ?>
 
-<h3>Ajouter genre </h3>
+<h1 class="mb-5 mt-5" >Ajouter genre</h1>
+
+<div class="container mb-5">
+    <div class="row g-0">
+        <form action="index.php?action=ajouterGenre" method="post">
+            <div class="row d-flex justify-content-evenly">
+                <div class="mb-5 col-4">
+                    <label for="formGroupExampleInput" class="form-label">Catégorie</label>
+                    <input class="form-control" type="text" name="nom_genre"  required>
+                </div>
+            </div>
+            <div class="d-flex justify-content-evenly">
+                <button class="btn btn-primary mb-5 " name="submit" type="submit">Ajouter</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+<!-- <h3>Ajouter genre </h3>
 <form action="index.php?action=ajouterGenre" method="post">
 
     <p>
@@ -17,7 +35,7 @@ ob_start();
     <p>
         <input type="submit" name="submit" value="Ajouter">
     </p>
-</form>    
+</form>     -->
 
 <?php
 
