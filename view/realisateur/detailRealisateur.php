1cbb878b997eff94d51db68bc41e2669b2dde785
Lunamorgendorffer <nabdou68@gmail.com>
2023-04-09 18:39:38 +0200

    style bootsrap

--- view/realisateur/detailRealisateur.php
+++ view/realisateur/detailRealisateur.php
@@ -3,13 +3,43 @@
 ob_start();
 
 ?>
-<h1>Detail realisateur</h1>
-<?php
-$realisateur= $realisateurs->fetch();
-    echo $realisateur['prenom']."  ";
-    echo $realisateur['nom']." ";
-    echo $realisateur['age']." ans <br> ";
-?>
+
+
+<h1>Detail Acteur</h1>
+
+<?php $realisateur= $realisateurs->fetch(); ?>
+
+<div class="container-flui d-flex justify-content-center mt-5">
+    <div class="row row-cols-md-5 g-4 mt-5 d-flex justify-content-evenly">
+        <div class="card mb-3 " style="max-width: 540px;">
+            <div class="col">
+                <div class="row g-0 ">
+                <!-- <img src="<?=$realisateur['image_realisateur'];?>" class="card-img-top" alt="..."> -->
+                    <div class="card-body">
+                        <p class="card-text"><strong>age </strong><?=$realisateur['age'];?></p>
+                        <p class="card-text"><strong>Sexe : </strong><?=$realisateur['sexe'];?></p>
+                        <p class="card-text"><strong>Nationalité :</strong> <?=$realisateur['nationalite'];?></p>
+                    </div>
+                    <hr>
+                    <div class="card-body">
+                        <p class="card-text">Biographie : <?="</br>".$realisateur['biographie'];?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <h2 class="card-title text-center">Filmographie</h2>
+                <ul class="list-group list-group-flush">
+                    <?php foreach ($castings->fetchAll() as $casting){?>
+                        <li class="list-group-item"><a href="index.php?action=detailFilm&id=<?= $casting['Id_film'] ?>"><?= $casting['titre'] ?></a></li>
+                    <?php } ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <?php
 
--- view/realisateur/listRealisateurs.php
+++ view/realisateur/listRealisateurs.php
@@ -3,14 +3,28 @@
 ob_start();
 
 ?>
-<h1>Bienvenue sur la page liste de realisateurs</h1>
-<?php
-while ($realisateur= $realisateurs->fetch()){?>
-    <a href="index.php?action=detailRealisateur&id=<?=$realisateur['id_realisateur']?>"><?=$realisateur['prenom']." ".$realisateur['nom']?></a><br>
-<?php
-    // echo $film['titre']."<br>";
-}
-?>
+
+<div class="container-fluid">
+    <h1>Bienvenue sur ma page realisateur</h1>
+
+    <div class="row row-cols-1 row-cols-md-6 g-4 mt-5 d-flex justify-content-evenly">
+        <?php foreach ($realisateurs->fetchAll() as $realisateur){ ?>
+            <div class="card m-2 mt-4 mb-4 bg-dark">
+                <div class="card-text">
+                    <h5 class="card-title text-center"><a href="index.php?action=detailRealisateur&id=<?=$realisateur['id_realisateur']?>"><?=$realisateur['prenom']." ".$realisateur['nom']?></a></h5><br>
+                </div>
+                <hr class="bg-light">
+                <div class="d-flex justify-content-center">
+                    <a href="index.php?action=detailRealisateur&id=<?=$realisateur['id_realisateur']?>"><button type="button" class="btn btn-outline-danger">Supprimer</button></a>
+                </div>
+            </div>
+        <?php }?>
+    </div>
+    <div class="row">
+        <div class="d-flex justify-content-evenly">
+        <a href="index.php?action=pageAjouterReal" class="btn btn-primary">Ajouter un realisateur</a>
+    </div>
+</div>
 
 
 <?php
