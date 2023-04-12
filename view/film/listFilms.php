c43ebcaec92b822353fb3b46c57f1b8e6f9f1cbe
Lunamorgendorffer <nabdou68@gmail.com>
2023-04-12 18:24:57 +0200

    Ajustement taille image

--- view/film/listFilms.php
+++ view/film/listFilms.php
@@ -3,32 +3,34 @@
 ob_start();
 
 ?>
-<h1>Bienvenue sur ma page film</h1>
 
-<div class="row row-cols-md-5 g-4 mt-5 d-flex justify-content-evenly">
-  <?php foreach ($films->fetchAll() as $film){?>
-    <div class="card m-2 mt-4 mb-4 bg-dark">
-      <figure>
-      <a class="detail-film card-img-top" href="index.php?action=detailFilm&id=<?= $film['id_film'] ?>"><?= $film['affiche'];?></a> 
-      </figure>
-      <div class="card-text">
-        <h5 class="card-title text-center">
-          <a href="index.php?action=detailFilm&id=<?= $film['id_film'] ?>"><?= $film['titre'] ?></a>
-        </h5>
+<div class="container">
+  <h1  class="display-1" style="text-align:center">Liste des films</h1>
+  <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 d-flex justify-content-evenly">
+    <?php foreach ($films->fetchAll() as $film){?>
+      <div class="col ">
+        <div class="card  border border-0 "  width="184" height="262">
+          <img src="<?= $film['affiche'];?>" class="rounded mx-auto d-block mt-3" alt="affiche du film">
+          <div class="card-body">
+            <p class="card-text text-center"><a href="index.php?action=detailFilm&id=<?= $film['id_film'] ?>"><?= $film['titre'] ?></a></p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group mx-auto ">
+                <a href="index.php?action=SupprimerFilm&id=<?=$film['id_film'] ?>"><button type="button" class="btn btn-secondary">Supprimer</button></a>
+              </div> 
+            </div>
+          </div>
+        </div>
       </div>
-      <hr class="bg-light">
-      <div class="d-flex justify-content-center">
-      <a href="index.php?action=deleteFilm&id=<?=$film['id_film'] ?>"><button type="button" class="btn btn-outline-danger">Supprimer</button></a>
-      </div>
-    </div>
-  <?php } ?>
-  <a href="index.php?action=viewAjouterFilm" class="btn btn-primary">Ajouter film</a>
+    <?php } ?>
+  </div>
+  <a href="index.php?action=viewAjouterFilm" class="btn btn-primary " >Ajouter film</a>
 </div>
+
 
 
 <?php
 
-$title="liste des films"; 
+$title="Films"; 
 $contenu = ob_get_clean(); // temporisation de sortie
 
 require "view/layout.php";
