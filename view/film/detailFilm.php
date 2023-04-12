8e984b775ac200db1a0ce1d39e0965df63593f4c
Lunamorgendorffer <nabdou68@gmail.com>
2023-04-12 19:39:57 +0200

    style bootstrap

--- view/film/detailFilm.php
+++ view/film/detailFilm.php
@@ -3,36 +3,45 @@
 ob_start();
 
 ?>
-<h1>Detail film</h1>
-<?php $film= $film->fetch(); ?>
-<div class="card-body">
-    <h2 class="card-title text-center"><?=$film['titre'];?></h2>
-    <p class="card-text text-center">Réalisé(e) par <?=$film['reali'];?></p>
-    <p class="card-text text-center">Durée: <?=$film['duree'];?> min</p>
-    <p class="card-text text-center"><small class="text-muted">Sortie le : <?=$film['dateDeSortie'];?></small></p>
-    <p class="card-text text-center"><?=$film['note'];?> sur 5</p>
-    <p class="card-text text-center"><strong>Synopsis : </strong> </p>
-    <p class="card-text"><?=$film['synopsis'];?></p>
-</div>
-
-<div class="col-4">
-    <div class="card mt-5" >
+<div class="container">
+    <?php $film= $film->fetch(); ?>
+    <div class="card-body text-center text-white " >
+        <h2 class="card-title "><?=$film['titre'];?></h2>
+        <p class="card-text">Réalisé(e) par <?=$film['reali'];?></p>
+        <p class="card-text">Durée: <?=$film['duree'];?> min</p>
+        <p class="card-text"><small>Sortie le : <?=$film['dateDeSortie'];?></small></p>
+        <p class="card-text"><?=$film['note'];?> sur 5</p>
+        <div class="d-flex justify-content-center">
+            <div class="card" style="width: 80%;">
+                <h5 class="card-title" style="text-align: center;">Synopsis :</h5>
+                <p class="text-dark mx-auto" ><b><?=$film['synopsis'];?></b></p>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center" >
+        <div class="card mt-3 " style="width: 78%;" >
             <h2 class="card-title text-center">Casting</h2>
             <ul class="list-group list-group-flush">
                 <?php foreach ( $casting->fetchAll() as $casting){ ?>
-                    <li class="list-group-item">
+                    <li class="list-group-item text-center">
                         <a href="index.php?action=detailActeur&id=<?= $casting['id_acteur']?>"><?= $casting['acteur'] ?></a>
                     </li>
                 <?php } ?>
             </ul>
         </div>
+    </div>
+    <div class=" mt-2 d-flex justify-content-center">
+        <a href="index.php?action=viewAjouterCasting&id=<?= $casting['id_film']?>"><button>Ajouter casting</button></a> 
+    </div>
 </div>
 
-<a href="index.php?action=viewAjouterCasting&id=<?= $casting['id_film']?>"><button>Ajouter casting</button></a>
+
+
+
 
 <?php
 
-$title="liste détail de film"; 
+$title="Détail du film"; 
 $contenu = ob_get_clean(); // temporisation de sortie
 
 require "view/layout.php";
